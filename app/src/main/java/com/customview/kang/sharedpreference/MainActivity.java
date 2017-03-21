7f99cfad9b5fbd2cf381e17208caf253fe52adf0
SOUTHNINE\kang <kangnk5@gmail.com>
2017-03-21 10:36:54 +0900

    setting view

--- app/src/main/java/com/customview/kang/sharedpreference/MainActivity.java
+++ app/src/main/java/com/customview/kang/sharedpreference/MainActivity.java
@@ -2,12 +2,30 @@ package com.customview.kang.sharedpreference;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editName;
+    Switch switchShuffle;
+
+    RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName = (EditText)findViewById(R.id.editText);
+        switchShuffle = (Switch)findViewById(R.id.switchShuffle);
+        layout = (RelativeLayout)findViewById(R.id.layout2);
+    }
+    public void closeHelp(View view){
+        layout.setVisibility(View.GONE);
+
+    }
+    public void saveSetting(View view){
+
     }
 }
